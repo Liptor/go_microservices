36f0222faeaeda9acae7f1603b19cb90f5ae2386
Ivan Ingovatov <ivan.ingov@gmail.com>
2024-04-18 23:10:51 +0300

    [FEAT]: add new enpoint to Services

---
+++ pkg/endpoints/endpoints.go
@@ -0,0 +1,100 @@
+package endpoints
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/aayushrangwala/watermark-service/internal"
+	"github.com/aayushrangwala/watermark-service/pkg/watermark"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
+)
+
+type Set struct {
+	GetEndpoint endpoint.Endpoint 
+	AddDocumentEndpoint endpoint.Endpoint
+	StatusEndpoint endpoint.Endpoint
+	ServiceStatusEndpoint endpoint.Endpoint
+	WatermarkEndpoint endpoint.Endpoint
+}
+
+func NewEndpointSet(svc watermark.Service) Set {
+	return Set{
+		GetEndpoint: MakeGetEndpoint(svc),
+		AddDocumentEndpoint: MakeGetEndpoint(svc),
+		StatusEndpoint: MakeGetEndpoint(svc),
+		ServiceStatusEndpoint: MakeGetEndpoint(svc),
+		WatermarkEndpoint: MakeGetEndpoint(svc),
+	}
+}
+
+func MakeGetEndpoint(svc watermark.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(GetRequest)
+		docs, err := svc.Get(ctx, req.Filters...)
+		if err != nil {
+			return GetResponse{docs, err.Error()}, nil
+		}
+		return GetResponse{docs, ""}, nil
+	}
+}
+
+func MakeStatusEndpoint(svc watermark.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(StatusRequest)
+		status, err := svc.Status(ctx, req.TicketID)
+		if err != nil {
+			return StatusResponse{Status: status, Err: err.Error()}, nil
+		}
+		return StatusResponse{Status: status, Err: ""}, nil
+	}
+}
+
+func MakeAddDocumentEndpoint(svc watermark.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(AddDocumentRequest)
+		tickedID, err := svc.AddDocument(ctx, req.Document)
+		if err != nil {
+			return AddDocumentResponse{TicketID: tickedID, Err: err.Error()}, nil
+		}
+		return AddDocumentResponse{TicketID: tickedID, Err: ""}, nil
+
+	}
+}
+
+func MakeWatermarkEndpoint(svc watermark.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(WatermarkRequest)
+		code, err := svc.Watermark(ctx, req.TicketID, req.Mark)
+		if err != nil {
+			return WatermarkResponse{Code: code, Err: err.Error()}, nil
+		}
+		return WatermarkResponse{Code: code, Err: ""}, nil
+
+	}
+}
+
+func MakeServiceStatusEndpoint(svc watermark.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface {}, error) {
+		_ = request.(ServiceStatusRequest)
+		code, err := svc.ServiceStatus(ctx)
+		if err != nil {
+			return ServiceStatusResponse{Code: code, Err: err.Error()}, nil
+		}
+		return ServiceStatusResponse{Code: code, Err: ""}, nil
+	}
+}
+
+func (s *Set) Get(ctx context.Context, filters ...internal.Filter) ([]internal.Document, error) {
+	resp, err := s.GetEndpoint(ctx, GetRequest{Filters: filters})
+	if err != nil {
+		return []internal.Document{}, err
+	}
+	getResp := resp.(GetResponse)
+	if getResp.Err != "" {
+		return []internal.Document{}, errors.New(getResp.Err)
+	}
+	return getResp.Documents, nil
+}
